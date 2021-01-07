388a2ca5778ec28b4f072d251f5bc683fdf44ee1
James Wampler <james@wamp.dev>
2021-01-06 19:23:15 -0800

    Adding first product integration test

--- tests/BuyEngine.WebApi.Tests.Integration/Catalog/ProductTests.cs
+++ tests/BuyEngine.WebApi.Tests.Integration/Catalog/ProductTests.cs
@@ -1,15 +1,23 @@
 using NUnit.Framework;
+using RestSharp;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace BuyEngine.WebApi.Tests.Integration.Catalog
 {
     public class ProductTests
     {
-        private readonly string _appBaseUrl = TestContext.Parameters["appBaseUrl"];
-        private readonly string _connectionString = TestContext.Parameters["connectionString"];
-        
+        private string _appBaseUrl;
+        private string _connectionString;
+        private RestClient _client;
+
         [SetUp]
         public void Setup()
         {
+            _appBaseUrl = TestContext.Parameters["apiBaseUrl"];
+            _connectionString = TestContext.Parameters["connectionString"];
+
+            _client = new RestClient(_appBaseUrl);
         }
 
         [Test]
@@ -25,9 +33,16 @@ namespace BuyEngine.WebApi.Tests.Integration.Catalog
         }
 
         [Test]
-        public void An_InventoryManager_Can_View_A_Product()
+        public async Task An_InventoryManager_Can_View_A_Product()
         {
-            Assert.Fail();
+            var request = new RestRequest("product/1", DataFormat.Json);
+            var response = await _client.ExecuteGetAsync(request);
+
+            Assert.That(response.IsSuccessful);
+
+            var json = JsonDocument.Parse(response.Content);
+            Assert.That(json, Is.Not.Null);
+            Assert.That(json.RootElement.GetProperty("id").GetInt32(), Is.EqualTo(1));
         }
 
         [Test]
